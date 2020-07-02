cdff15848508056b21a2359ec6416ed439f06ddd
AbdelRochdi <abdelghafour.rochdi96@gmail.com>
2020-07-03 00:38:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -105,7 +105,7 @@
  // Create Developpement connection
 //  $con=mysqli_connect("localhost","root","","testy");
  // Create Remote connection
- $con=mysqli_connect("sql7.freemysqlhosting.net","sql7352240","u58Muyr19M","sql7352240");
+ $con=mysqli_connect("sql7.freemysqlhosting.net","sql7352240","u58Muyr19","sql7352240");
  
  // Check connection
  if (mysqli_connect_errno())
@@ -143,8 +143,6 @@
   
      <?php
      
- }else {
-     die('query failed');
  }
   
  mysqli_close($con);
