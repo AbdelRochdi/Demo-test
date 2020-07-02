ab3cab048c2f20ed6ae2605d997872651b8d38bc
AbdelRochdi <abdelghafour.rochdi96@gmail.com>
2020-07-02 23:43:23 +0100

    remote

--- insert.php
+++ insert.php
@@ -1,7 +1,9 @@
 <?php
  
- // Create connection
- $con=mysqli_connect("localhost","root","","testy");
+ // Create Developpement connection
+//  $con=mysqli_connect("localhost","root","","testy");
+ // Create Remote connection
+ $con=mysqli_connect("sql7.freemysqlhosting.net","sql7352240","u58Muyr19M","sql7352240");
  
  // Check connection
  if (mysqli_connect_errno())
